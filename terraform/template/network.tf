b56227fb7523f1ce5cc5b4ec4a1a9309a4ed0f31
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-26 17:39:06 +0530

    update

--- terraform/template/network.tf
+++ terraform/template/network.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "prod-vpc" {
  }
  
  resource "aws_subnet" "subnet-efs" {
-   cidr_block = "${cidrsubnet(aws_vpc.prod-vpc.cidr_block, 8, 8)}"
-   vpc_id = "${aws_vpc.prod-vpc.id}"
+   cidr_block = cidrsubnet(aws_vpc.prod-vpc.cidr_block, 8, 8)
+   vpc_id = aws_vpc.prod-vpc.id
    availability_zone = "us-east-1a"
  }
