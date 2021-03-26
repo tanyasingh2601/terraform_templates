eb307f8d5331cd381f57774ae3a1c9da6127f3d7
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-26 17:37:37 +0530

    update

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -9,7 +9,7 @@ resource "aws_efs_file_system" "efs-example" {
  }
 
  resource "aws_efs_mount_target" "efs-mt-example" {
-   file_system_id  = "${aws_efs_file_system.efs-example.id}"
-   subnet_id = "${aws_subnet.subnet-efs.id}"
-   security_groups = ["${aws_security_group.ingress-efs-test.id}"]
+   file_system_id  = aws_efs_file_system.efs-example.id
+   subnet_id = aws_subnet.subnet-efs.id
+   security_groups = [aws_security_group.ingress-efs-test.id]
  }
