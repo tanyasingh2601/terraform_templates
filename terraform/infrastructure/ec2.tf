05cdd55a792ca35ab578bf1aed6f34756a60e330
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-31 02:08:14 +0530

    Create ec2.tf

---
+++ terraform/infrastructure/ec2.tf
@@ -0,0 +1,51 @@
+resource "aws_instance" "prod-web" {
+    count = var.resource_count[var.profile]
+    ami = var.instance_ami
+    instance_type = var.instance-type
+    subnet_id = aws_subnet.prod-subnet-public.id
+    vpc_security_group_ids = [ aws_security_group.prod-public-sg.id ]
+    key_name = "terraform-default"
+    provisioner "remote-exec" {
+        inline = [
+             "sudo apt-get update",
+             "sudo apt-get install apache2 -y"
+        ]
+    }
+    connection {
+        host = self.public_ip
+        user = var.EC2_USER
+        private_key = file(var.PRIVATE_KEY_PATH_WEB)
+    }
+    tags = {
+      Name = "prod-web"
+    }
+}
+
+
+resource "aws_instance" "prod-bastion" {
+  ami           = var.instance_ami
+  instance_type = var.instance-type
+
+  subnet_id = aws_subnet.prod-subnet-public.id
+
+  vpc_security_group_ids = [ aws_security_group.prod-private-sg.id ]
+
+  key_name = "terraform-default"
+
+  tags = {
+    Name = "prod-bastion"
+  }
+}
+
+resource "aws_instance" "prod-backend" {
+    # count = var.resource_count[var.profile]
+    ami = var.instance_ami
+    instance_type = var.instance-type
+    subnet_id = aws_subnet.prod-subnet-private.id
+    vpc_security_group_ids = [ aws_security_group.prod-private-sg.id ]
+    key_name = "terraform-default"
+
+    tags = {
+      Name = "prod-backend"
+    }
+}
