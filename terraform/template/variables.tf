ec30743f9a79933cb1f546584db05e1e2255edde
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-30 10:21:27 +0530

    updated var.tf

--- terraform/template/variables.tf
+++ terraform/template/variables.tf
@@ -1,3 +1,23 @@
+variable "aws_region" {
+  type = string
+  description = "AWS region to launch servers."
+}
+
+variable "aws_zone" {
+  type = string
+  description = "AWS zone to create subnet."
+}
+
+variable "access_key" {
+  type = string
+  description = "Access key for AWS"
+}
+
+variable "secret_key" {
+  type = string
+  description = "Secret key for AWS"
+}
+
 variable "ami" {
   type = string
   default = "ami-0915bcb5fa77e4892"
@@ -17,7 +37,7 @@ variable "security_groups" {
   default = ["sg-084a0ee96cd94f837"]
 }
 
-# variable "key" {
-#   type = string
-#   default = "shubham-key12345"
-# }
+variable "key" {
+  type = string
+  default = "shubham-key12345"
+}
