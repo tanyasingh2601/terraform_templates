22992de342c4788c481645c13378800fb1618b70
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-30 10:31:28 +0530

    group

--- terraform/template/variables.tf
+++ terraform/template/variables.tf
@@ -18,26 +18,7 @@ variable "secret_key" {
   description = "Secret key for AWS"
 }
 
-variable "ami" {
-  type = string
-  default = "ami-0915bcb5fa77e4892"
-}
-
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
-}
-
-variable "subnet_id" {
-  type = string
-  default = "subnet-2e143663"
-}
-
-variable "security_groups" {
-  default = ["sg-084a0ee96cd94f837"]
-}
-
-variable "key" {
-  type = string
-  default = "shubham-key12345"
+variable "user" {
+  type = list(string)
+  default = ["developers", "testers", "production"]
 }
