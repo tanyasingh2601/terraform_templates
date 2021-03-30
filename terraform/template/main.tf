5bdddd4ed8f34f49b6f3271cb8f64d341d694602
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-30 10:30:05 +0530

    group

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -1,34 +1,32 @@
-resource "aws_instance" "instance" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  subnet_id       = var.subnet_id
-  # key_name        = var.key
-  user_data       = data.template_file.script.rendered
-  security_groups = var.security_groups
-tags = {
-    Name      = "EFS_TEST"
-    Terraform = "true"
-  }
-volume_tags = {
-    Name      = "EFS_TEST_ROOT"
-    Terraform = "true"
-  }
+resource "aws_iam_group" "my_developers" {
+  name = var.user[0]
+  path = "/"
 }
-resource "aws_efs_file_system" "efs" {
-  creation_token   = "EFS Shared Data"
-  performance_mode = "generalPurpose"
-tags = {
-    Name = "EFS Shared Data"
-  }
+
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.my_developers.name
+  policy = file("policydeveloper.json")
 }
-resource "aws_efs_mount_target" "efs" {
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = var.subnet_id
-  security_groups = var.security_groups
+
+resource "aws_iam_group" "my_testers" {
+  name = var.user[1]
+  path = "/"
 }
-data "template_file" "script" {
-  template = file("script.tpl")
-  vars = {
-    efs_id = aws_efs_file_system.efs.id
-  }
+
+resource "aws_iam_group_policy" "my_tester_policy" {
+  name  = "my_tester_policy"
+  group = aws_iam_group.my_testers.name
+  policy = file("policytester.json")
+}
+
+resource "aws_iam_group" "my_production" {
+  name = var.user[2]
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "my_production_policy" {
+  name  = "my_production_policy"
+  group = aws_iam_group.my_production.name
+  policy = file("policyproduction.json")
 }
