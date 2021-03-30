97b2c48c40a3651567520de967945b9488ee50ae
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-30 11:17:33 +0530

    update main.tf

--- terraform/template/main.tf
+++ terraform/template/main.tf
@@ -1,32 +1,46 @@
-resource "aws_iam_group" "my_developers" {
-  name = var.user[0]
-  path = "/"
+provider "aws" {
+ region = "us-east-1" 
 }
 
-resource "aws_iam_group_policy" "my_developer_policy" {
-  name  = "my_developer_policy"
-  group = aws_iam_group.my_developers.name
-  policy = file("policydeveloper.json")
+#AWS Instance
+resource "aws_instance" "exampleEBS" {
+    ami = "ami-03d315ad33b9d49c4"
+    instance_type = "t2.micro"
+    availability_zone = var.availability_zone
+  
+  lifecycle {
+    ignore_changes = [ami]
+  }
 }
 
-resource "aws_iam_group" "my_testers" {
-  name = var.user[1]
-  path = "/"
+#EBS Volume and Attachment
+
+resource "aws_ebs_volume" "exampleEBS" {
+  availability_zone = var.availability_zone
+  size              = 40
 }
 
-resource "aws_iam_group_policy" "my_tester_policy" {
-  name  = "my_tester_policy"
-  group = aws_iam_group.my_testers.name
-  policy = file("policytester.json")
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.exampleEBS.id
+  instance_id = aws_instance.exampleEBS.id
 }
 
-resource "aws_iam_group" "my_production" {
-  name = var.user[2]
-  path = "/"
-}
+#Cloudwatch Metric
 
-resource "aws_iam_group_policy" "my_production_policy" {
-  name  = "my_production_policy"
-  group = aws_iam_group.my_production.name
-  policy = file("policyproduction.json")
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+  alarm_name                = "cpu-utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2" 
+  period                    = "120" #seconds
+  statistic                 = "Average"
+  threshold                 = "80" 
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+
+  dimensions = {
+        InstanceId = aws_instance.exampleEBS.id
+      }
 }
