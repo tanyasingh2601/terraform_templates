14bf6ebc49d83298cfe0bb18d633e54efc959514
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-30 14:52:46 +0530

    Create main.tf

---
+++ terraform/group/main.tf
@@ -0,0 +1,39 @@
+# Specify the provider and access details
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_iam_group" "my_developers" {
+  name = var.user[0]
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "my_developer_policy" {
+  name  = "my_developer_policy"
+  group = aws_iam_group.my_developers.name
+  policy = file("policydeveloper.json")
+}
+
+resource "aws_iam_group" "my_testers" {
+  name = var.user[1]
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "my_tester_policy" {
+  name  = "my_tester_policy"
+  group = aws_iam_group.my_testers.name
+  policy = file("policytester.json")
+}
+
+resource "aws_iam_group" "my_production" {
+  name = var.user[2]
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "my_production_policy" {
+  name  = "my_production_policy"
+  group = aws_iam_group.my_production.name
+  policy = file("policyproduction.json")
+}
