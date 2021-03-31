60f510992b7f8a293d664c4f2428a65d82aa5037
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-31 18:43:38 +0530

    Delete outputs.tf

--- terraform/infrastructure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_ip" {
-    value = aws_instance.prod-web.public_ip
-    description = "IP on which apache server is running"
-}
-
-output "backend_ip" {
-    value = aws_instance.prod-backend.public_ip
-    description = "Backend IP"
-}
