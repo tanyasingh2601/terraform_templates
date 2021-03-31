8a2729b9fd7f5fbddd336d33e94b1042ffc23418
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-31 09:28:54 +0530

    Update variables.tf

--- terraform/createUser/variables.tf
+++ terraform/createUser/variables.tf
@@ -1,6 +1,27 @@
+variable "aws_region" {
+  type = string
+  description = "AWS region to launch servers."
+}
+
+variable "aws_zone" {
+  type = string
+  description = "AWS zone to create subnet."
+}
+
+variable "access_key" {
+  type = string
+  description = "Access key for AWS"
+}
+
+variable "secret_key" {
+  type = string
+  description = "Secret key for AWS"
+}
+
 variable "profile" {
     default = "developers"
 }
+
 variable "username"{
     default = "Tanya"
 }
