82c0b6e501c8f6741488f37bff51cff0142a087d
tanyasingh2601 <33540810+tanyasingh2601@users.noreply.github.com>
2021-03-31 18:56:55 +0530

    Update vpc.tf

--- terraform/infrastructure/vpc.tf
+++ terraform/infrastructure/vpc.tf
@@ -1,3 +1,10 @@
+# Specify the provider and access details
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 resource "aws_vpc" "prod-vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_support = "true" 
